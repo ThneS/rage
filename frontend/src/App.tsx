648ce2bc595e1d24a1951c7fbd7cc96037335dca
Thne <thneonl@outlook.com>
2025-06-11 20:30:10 +0800

    使用 RouterProvider 替换了原来的 BrowserRouter 组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import DocumentLoader from './pages/DocumentLoader';
 import DocumentSplitter from './pages/DocumentSplitter';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/document-loader" replace />
+      },
+      {
+        path: 'document-loader',
+        element: <DocumentLoader />
+      },
+      {
+        path: 'document-splitter',
+        element: <DocumentSplitter />
+      },
+      {
+        path: '*',
+        element: <Navigate to="/document-loader" replace />
+      }
+    ]
+  }
+]);
+
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Navigate to="/document-loader" replace />} />
-          <Route path="document-loader" element={<DocumentLoader />} />
-          <Route path="document-splitter" element={<DocumentSplitter />} />
-          {/* 其他路由将在后续添加 */}
-          <Route path="*" element={<Navigate to="/document-loader" replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
